f15969b9458fc9a6b22d18897b991817ba55654c
igorrmotta <igor@edapp.com>
2021-09-08 21:23:17 +1000

    initial database schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,24 @@
 import { ImageProcessor } from "./image-processor";
 import { paths } from "./paths";
+import { PrismaClient } from "@prisma/client";
 
-const img = new ImageProcessor(paths.sampleImage);
-img
-  .applyWatermark()
-  .then(() => console.log("watermarked"))
-  .catch((err) => console.error(err));
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+
+  const img = new ImageProcessor(paths.sampleImage);
+  img
+    .applyWatermark()
+    .then(() => console.log("watermarked"))
+    .catch((err) => console.error(err));
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
